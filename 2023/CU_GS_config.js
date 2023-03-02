6904ac758b1b44f010f6b8d434a5ab6374c56204
jasonlin1222 <jasonlin1222@gmail.com>
2023-03-02 10:48:58 +0800

    add manifest

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023test",
+      "defaultValue": "2023AZGL",
       "required": "true",
       "disabled": "true"
     },
